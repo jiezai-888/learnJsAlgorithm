fd95e913d572ba9aabb83638af8b7771acc56612
jiezai-888 <3435941500@qq.com>
2021-10-30 06:19:10 +0800

    Function的apply方法

---
+++ js/FunctionAPI/myapply.js
@@ -0,0 +1,31 @@
+//Function.prototype.apply(thisarg,argArray)
+
+function apply(thisarg){
+	
+	//thisarg不存在指向window
+	let func=thisarg;
+	if(thisarg){
+		func=Object(func);
+	}else{
+		func=window;
+	}
+	func['fn']=this;
+	//参数数组
+	let argArray=arguments[1];
+	//参数不是数组
+	if(!Array.isArray(argArray)){
+		throw new TypeError("参数不是一个数组")
+	}
+	if(!argArray){
+		return func.fn();
+	}else{
+		return func.fn(...argArray);
+	}
+	delete func.fn;
+}
+
+Function.prototype.myapply=apply;
+
+
+
+
