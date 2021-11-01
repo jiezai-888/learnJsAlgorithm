32ca53e46ff78faae3ff9eb52e09eb27d84050a8
jiezai-888 <3435941500@qq.com>
2021-11-01 18:04:50 +0800

    Function的apply

--- js/FunctionAPI/myapply.js
+++ js/FunctionAPI/myapply.js
@@ -13,7 +13,7 @@ function apply(thisarg){
 	//参数数组
 	let argArray=arguments[1];
 	//参数不是数组
-	if(!Array.isArray(argArray)){
+	if(!Array.isArray(argArray) && argArray){
 		throw new TypeError("参数不是一个数组")
 	}
 	if(!argArray){
